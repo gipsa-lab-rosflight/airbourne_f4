10b52fd8085e0c9d5b3054c70b1ccb000567ae0c
Amaury Negre <amaury.negre@gipsa-lab.fr>
2019-06-26 09:32:05 +0200

    change ublox baudrate management

--- src/ublox.cpp
+++ src/ublox.cpp
@@ -38,15 +38,15 @@ void UBLOX::init(UART *uart)
 
 
   //serial_->set_mode(115200, UART::MODE_8N1);
-  serial_->set_mode(9600, UART::MODE_8N1);
+  serial_->set_mode(57600, UART::MODE_8N1);
   serial_->register_rx_callback(cb);
 
-  //if (!detect_baudrate())
-  //  return;
+  if (!detect_baudrate())
+    return;
 
   // Otherwise, Configure the GNSS receiver
   //set_baudrate(115200);
-  set_baudrate(9600);
+  //set_baudrate(57600);
   set_dynamic_mode();
   set_nav_rate(100);
   enable_message(CLASS_NAV, NAV_PVT, 1);
